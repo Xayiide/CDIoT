e3c35ed0cf426dac1e1bfe209fcf43049429d97a
Xayiide <wickedcheerio@protonmail.com>
2021-05-04 01:27:31 +0200

    :octopus:

--- esp32/src/main/main.ino
+++ esp32/src/main/main.ino
@@ -145,9 +145,9 @@ bool checkUser(String c) {
 }
 
 
- /* 0  -> D3
-    1  -> D10
-    2  -> D4
+ /* 0  -> D3  -> FLASH buttom
+    1  -> D10 
+    2  -> D4  -> MCULED
     3  -> D9
     4  -> D2
     5  -> D1
@@ -155,4 +155,5 @@ bool checkUser(String c) {
     13 -> D7
     14 -> D5
     15 -> D8
-    16 -> D0 */
+    16 -> D0  -> ESPLED
+  */
